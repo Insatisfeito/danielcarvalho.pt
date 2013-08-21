b8d9b85e8dce14e1d887ebed5f19250eaeecc094
Daniel Carvalho <dapcarvalho@gmail.com>
2013-08-21 05:52:30 +0100

    Repo cleanup and start over

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DanielcarvalhoPt::Application.config.secret_key_base = 'c9e4dfc993ce26f28eebf4642d1dffa840f9e1312069d6d923743e200d0c984a251e62c6e6d397c5efcbf3c71ed9575960ab107a3f3f32132a8b88853ce4743d'
